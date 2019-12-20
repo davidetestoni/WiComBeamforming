1c4681232b43762eacb76c905e9a0d4eda8e21dd
Stefano Moro <morostefano96@gmail.com>
2019-12-20 17:25:35 +0100

    added power managing and solved normalization of power

--- main.m
+++ main.m
@@ -5,16 +5,16 @@ rng(2014);
 gc = set2DParameters();
 
 % Tunable parameters
-tp.snr = 10;
+tp.tx_pow = 20; %dbm
 
-tp.tgt(1).range = 2750;    % m
-tp.tgt(1).angle = 0;       % degrees
-tp.tgt(2).range = 2750;    % m
+tp.tgt(1).range = 500;    % m
+tp.tgt(1).angle = 7;       % degrees
+tp.tgt(2).range = 500;    % m
 tp.tgt(2).angle = 10;       % degrees
 
-tp.intf(1).range = 9000;    % m
+tp.intf(1).range = 1000;    % m
 tp.intf(1).angle =   26;    % degrees
-tp.intf(2).range = 9000;    % m
+tp.intf(2).range = 1000;    % m
 tp.intf(2).angle =   -50;    % degrees
 
 tp.num_tx_el = 16;       
@@ -36,9 +36,14 @@ arrayresp = phased.ArrayResponse('SensorArray',ula,'WeightsInputPort',true);
 
 %% Signal generation
 [data_bit,data_sample,data_qam,cp_sig] = modulateData(gc,tp);
+% Normalization
+norm_factor = mean(abs(cp_sig));
+cp_sig = cp_sig ./ norm_factor;
 
+% Amplification
+cp_sig = cp_sig .* 10^(0.5 * tp.tx_pow/10);
 
-%% DOA estimation
+%% Signal Collection
 % target and interferer send signaling data to allow discovery
 
 tp.tgt(1).tx_sig = cp_sig(:,:,1);
@@ -55,17 +60,22 @@ tp.intf(2).tx_sig = cp_sig(:,:,33);
 tp.intf(2).data_bit = data_bit(:,:,33);
 
 %free space loss
-tp.tgt(1).rx_sig = tp.tgt(1).tx_sig.* 10^-(fspl(tp.tgt(1).range,gc.lambda)/10);
-tp.tgt(2).rx_sig = tp.tgt(2).tx_sig.* 10^-(fspl(tp.tgt(2).range,gc.lambda)/10);
-tp.intf(1).rx_sig = tp.intf(1).tx_sig.* 10^-(fspl(tp.intf(1).range,gc.lambda)/10);
-tp.intf(2).rx_sig = tp.intf(2).tx_sig.* 10^-(fspl(tp.intf(2).range,gc.lambda)/10);
+tp.tgt(1).rx_sig = tp.tgt(1).tx_sig.* 10^(-0.5*(fspl(tp.tgt(1).range,gc.lambda)/10));
+tp.tgt(2).rx_sig = tp.tgt(2).tx_sig.* 10^(-0.5*(fspl(tp.tgt(2).range,gc.lambda)/10));
+tp.intf(1).rx_sig = tp.intf(1).tx_sig.* 10^(-0.5*(fspl(tp.intf(1).range,gc.lambda)/10));
+tp.intf(2).rx_sig = tp.intf(2).tx_sig.* 10^(-0.5*(fspl(tp.intf(2).range,gc.lambda)/10));
 
 all_sig = [tp.tgt(1).rx_sig tp.tgt(2).rx_sig tp.intf(1).rx_sig tp.intf(2).rx_sig];
 rx = collectPlaneWave(ula,all_sig,[tp.tgt(1).angle tp.tgt(2).angle  tp.intf(1).angle tp.intf(2).angle; 0 0 0 0],gc.fc);
 %rx = collectPlaneWave(ula,[tp.tgt(1).rx_sig tp.tgt(2).rx_sig],[tp.tgt(1).angle tp.tgt(2).angle;0 0],gc.fc);
 
-rx = awgn(rx,tp.snr,'measured');
+noise = sqrt(physconst('Boltzmann') * gc.nT * gc.B / 2) * randn(size(rx));
+rx = rx + noise;
+total_noise = mean(mean(abs(noise).^2))+ mean(abs(tp.tgt(2).rx_sig).^2) + mean(abs(tp.intf(1).rx_sig).^2)+ mean(abs(tp.intf(2).rx_sig).^2);
+snr = 10*log10(mean(abs(tp.tgt(1).rx_sig).^2)/ total_noise);
+%rx = awgn(rx,tp.snr,'measured');
 
+%% DOA estimation
 musicEstimator = phased.MUSICEstimator('SensorArray',ula,...
         'OperatingFrequency',gc.fc,'ScanAngles',gc.scan_az,...
         'DOAOutputPort',true,'NumSignalsSource','Property','NumSignals',4);
@@ -90,14 +100,31 @@ for j = 1 :size(steer_vec,2)
         y(i,j) = w' * transpose(rx(i,:));
     end
 end
+
+%% amplify data
+ampl_gain(1) = 10*log10(mean(abs(tp.tgt(1).tx_sig).^2) / mean(abs(y(:,1).^2)));
+ampl_gain(2) = 10*log10(mean(abs(tp.tgt(2).tx_sig).^2)/ mean(abs(y(:,2).^2)));
+
+y_amp(:,1) = y(:,1) .* 10^( 0.5 * ampl_gain(1)/10);
+y_amp(:,2) = y(:,2) .* 10^( 0.5 * ampl_gain(2)/10);
+% re-normalize
+y_norm(:,1) = y_amp(:,1) ./ mean(abs(y_amp(:,1)));
+y_norm(:,2) = y_amp(:,2) ./ mean(abs(y_amp(:,2)));
+y_amp = y_norm .* norm_factor;
+
+
+
 %% demodulate ofdm symbol
+
 y_cap = zeros(size(data_qam,1),num_tgt);
 y_sample = zeros(size(data_qam,1),num_tgt);
 for i = 1:size(y_cap,2)
-    y_cap(:,i) = ofdm_demod(y(:,i),gc.N_fft,gc.N_cyclepref,gc.pilot_pos,gc.guard_bands);
+    y_cap(:,i) = ofdm_demod(y_amp(:,i),gc.N_fft,gc.N_cyclepref,gc.pilot_pos,gc.guard_bands);
     y_sample(:,i) = qamdemod(y_cap(:,i),gc.M);
 end
 
+
+%% BER
 [~,ber(1)] = biterr(de2bi(y_sample(:,1)),tp.tgt(1).data_bit);
 [~,ber(2)] = biterr(de2bi(y_sample(:,2)),tp.tgt(2).data_bit);
 [~,ber_c(1)] = biterr(de2bi(y_sample(:,2)),tp.tgt(1).data_bit);
--- set2DParameters.m
+++ set2DParameters.m
@@ -3,7 +3,7 @@ function gc = set2DParameters
 
 gc.up_sample = 8; %?
 gc.c_light = 3e8;
-gc.fc = 2.4e9;               % 2.4 GHz ISM Band
+gc.fc = 26e9;               % 2.4 GHz ISM Band
 gc.lambda = gc.c_light/gc.fc;
 gc.nT = 290;       % Noise Temp in deg K
 gc.scan_az = -90:90;
@@ -11,9 +11,11 @@ gc.scan_az = -90:90;
 % modulation
 gc.M = 16;
 gc.N_fft = 64;
+gc.delta_f = 15e3;
 gc.pilot_pos = [12;26;40;54];
 gc.pil = 3+3j;
 gc.guard_bands = [6,5];
 gc.N_bit_block = (gc.N_fft - max(size(gc.pilot_pos)) - sum(gc.guard_bands)) * log2(gc.M); %block is 1 ofdm symbol
 gc.N_cyclepref = 16;
 gc.N_carried_data = gc.N_fft - numel(gc.pilot_pos) - sum(gc.guard_bands);
+gc.B = (gc.N_fft -sum(gc.guard_bands))* gc.delta_f;
