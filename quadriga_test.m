e54345ac4548b09e353c7fb4ab62466da81071de
= <=>
2020-03-02 19:21:15 +0100

    fix pos

--- quadriga_test.m
+++ quadriga_test.m
@@ -4,17 +4,18 @@ clc
 
 t = qd_track('linear',200,0);
 t.name = 'Terminal';
-t.initial_position(3,1) = 2;      
+t.initial_position = [100,100,2];      
 t.calc_orientation;
 l = qd_layout;   
 
 [~,l.tx_track] = interpolate( t.copy,'distance',0.1 );
+l.rx_position = [0,0,15];
+
 l.visualize([],[],0);    
 axis equal
 title('Track layout');
 
 %%
-l.rx_position = [0,0,15];
 l.simpar.center_frequency = 26e9; 
 l.rx_array = qd_arrayant( '3gpp-3d', 4, 4, l.simpar.center_frequency);
 l.rx_track.orientation = [ 0 ; 0 ; 0]*pi/180;
